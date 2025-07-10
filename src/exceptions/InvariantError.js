1e4d0a6f006c780783c5ab2cbb494066fd187341
Mochamed Fadhlan Tuhairi <fadhlantuhairi@gmail.com>
2025-07-10 11:23:32 +0700

    Add: Custome error for spesific Error messages

---
+++ src/exceptions/InvariantError.js
@@ -0,0 +1,11 @@
+const ClientError = require("./ClientError");
+
+class InvariantError extends ClientError {
+    constructor(messages) {
+        super(messages);
+        this.name = 'InvariantError';
+        
+    }
+}
+
+module.exports = InvariantError;
