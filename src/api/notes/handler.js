273e0a8db067f77eea90f7341d6b124fbfce3093
Mochamed Fadhlan Tuhairi <fadhlantuhairi@gmail.com>
2025-07-31 15:13:42 +0700

    Update service to use verifyNoteAccess

--- src/api/notes/handler.js
+++ src/api/notes/handler.js
@@ -78,7 +78,7 @@ class NotesHandler {
     const { id } = request.params;
     const { id: credentialId } = request.auth.credentials;
 
-    await this._service.verifyNoteOwner(id, credentialId);
+    await this._service.verifyNoteAccess(id, credentialId);
     const note = await this._service.getNoteById(id);
     
     return {
@@ -113,7 +113,7 @@ class NotesHandler {
     const { id } = request.params;
     const { id: credentialId } = request.auth.credentials;
 
-    await this._service.verifyNoteOwner(id, credentialId);
+    await this._service.verifyNoteAccess(id, credentialId);
     await this._service.editNoteById(id, request.payload);
 
       return {
