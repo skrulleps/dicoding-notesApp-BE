545df6126e3dd83b156d943a6be8637a927d71d2
Mochamed Fadhlan Tuhairi <fadhlantuhairi@gmail.com>
2025-08-08 14:28:24 +0700

    Add: method for client get file from server

--- src/api/uploads/routes.js
+++ src/api/uploads/routes.js
@@ -1,3 +1,5 @@
+const path = require('path');
+
 const routes = (handler) => [
     {
         method: 'POST',
@@ -11,6 +13,15 @@ const routes = (handler) => [
             },
         },
     },
+    {
+        method: 'GET',
+        path: '/upload/{param*}',
+        handler: {
+            directory: {
+                path: path.resolve(__dirname, 'file'),
+            }
+        }
+    }
 ];
 
 module.exports = routes;
